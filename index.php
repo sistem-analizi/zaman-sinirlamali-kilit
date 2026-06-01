2a9b0bb6b44acfe73b34b4bc1e351b469779d1e5
mertakcilll <240928008@ogr.comu.edu.tr>
2026-06-01 13:57:05 +0300

    Add files via upload
    Backend API ve CI/CD eklendi

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Kreait\Firebase\Factory;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$app = AppFactory::create();
+
+$factory = (new Factory)
+    ->withServiceAccount(__DIR__ . '/' . $_ENV['FIREBASE_CREDENTIALS'])
+    ->withDatabaseUri('https://zamanlamali-kilit-default-rtdb.europe-west1.firebasedatabase.app');
+$database = $factory->createDatabase();
+
+$app->get('/api/sistem-durumu', function ($request, $response, $args) use ($database) {
+    $reference = $database->getReference('sistem_durumu');
+    $value = $reference->getValue();
+
+    $response->getBody()->write(json_encode($value));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
